56fe2792f0bd8d9ddd527473b016864523308366
RoushAH <Roush.Adam.H@gmail.com>
2025-01-12 15:41:16 +0000

    #9 Config builder...built!

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # olive_garden_lights
-Power and control the string of lights across the garden wall
+Power and control the string of lights across the garden wall, using a Pi Pico W in micropython
 
-# notes
-As this is a public repo, I need to put certain config details into config.ini. This will protect privacy. I'm sure you understand.
+# Installation notes
+In the interest of privacy, I have provided `make_config.py`. This is written in standard python.
+
+This needs to be run on a parent computer prior to loading into the pico. It will ask for relevant details and generate the corresponding JSON.
+
+Please ensure the JSON is copied across. `make_config.py` does _not_ need to be on the pico
---
+++ make_config.py
@@ -0,0 +1,44 @@
+import json
+
+class Setting:
+    def __init__(self, name, this_type, maximum=None, minimum=None):
+        self.name = name
+        self.type = this_type
+        self.max = maximum
+        self.min = minimum
+        self.value = None
+
+    def set_value(self, value):
+        if self.type != str:
+            try:
+                value = self.type(value)
+            except ValueError:
+                print(f"Value of type {type(value)} is not {self.type}")
+                return False
+        if self.type in [float, int] and (value < self.min or value > self.max):
+            print(f"Value out of range ({self.min}, {self.max})")
+            return False
+        self.value = value
+        return True
+
+    def __str__(self):
+        return f"{self.name}: {str(self.value)}"
+
+settings = [
+    Setting("SSID", str),
+    Setting("Password", str),
+    Setting("Latitude", float, 90, -90),
+    Setting("Longitude", float, 180, -180),
+    Setting("Light_Sensitivity", int, 100, 0)
+]
+
+for setting in settings:
+    val = input(f"Enter value for {setting.name}: ")
+    if not(setting.set_value(val)):
+        val = input(f"Enter value for {setting.name}: ")
+
+setting_vals = {x.name:x.value for x in settings}
+print(json.dumps(setting_vals, indent=3))
+with open("config.json", "w") as f:
+    json.dump(setting_vals, f, indent=3)
+
